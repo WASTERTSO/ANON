fc18d1ff9f458e2d712ac9abf131be1e240314d3
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2023-07-12 13:10:39 +0530

    Update userbot.py

--- AnonX/core/userbot.py
+++ AnonX/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.one.join_chat("full_on_bakchodii")
+                await self.one.join_chat("makima_updatess")
             except:
                 pass
             assistants.append(1)
@@ -62,7 +62,7 @@ class Userbot(Client):
 
             try:
                 await self.one.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴏɴᴇ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.one.id}`\n❄ ɴᴀᴍᴇ : {self.one.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.one.username}"
+                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} 𝐴𝑠𝑠𝑖𝑠𝑡𝑎𝑛𝑡 𝑂𝑛𝑒 𝑆𝑡𝑎𝑟𝑡𝑒𝑑 :**\n\n✨ 𝐼𝑑 : `{self.one.id}`\n❄ 𝑁𝑎𝑚𝑒 : {self.one.name}\n💫 𝑈𝑠𝑒𝑟𝑛𝑎𝑚𝑒 : @{self.one.username}"
                 )
             except:
                 LOGGER(__name__).error(
@@ -75,8 +75,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DevilsHeavenMF")
-                await self.two.join_chat("FallenAssociation")
+                await self.two.join_chat("Full_on_bakchodii")
+                await self.two.join_chat("makima_updatess")
             except:
                 pass
             assistants.append(2)
@@ -89,7 +89,7 @@ class Userbot(Client):
 
             try:
                 await self.two.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴛᴡᴏ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.two.id}`\n❄ ɴᴀᴍᴇ : {self.two.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.two.username}"
+                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} 𝐴𝑠𝑠𝑖𝑠𝑡𝑎𝑛𝑡 𝑇𝑤𝑜 𝑆𝑡𝑎𝑟𝑡𝑒𝑑 :**\n\n✨ 𝐼𝑑 : `{self.two.id}`\n❄ 𝑁𝑎𝑚𝑒 : {self.two.name}\n💫 𝑈𝑠𝑒𝑟𝑛𝑎𝑚𝑒 : @{self.two.username}"
                 )
             except:
                 LOGGER(__name__).error(
@@ -102,8 +102,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("DevilsHeavenMF")
-                await self.three.join_chat("FallenAssociation")
+                await self.three.join_chat("full_on_bakchodii")
+                await self.three.join_chat("makima_updatess")
             except:
                 pass
             assistants.append(3)
@@ -116,7 +116,7 @@ class Userbot(Client):
 
             try:
                 await self.three.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴛʜʀᴇᴇ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.three.id}`\n❄ ɴᴀᴍᴇ : {self.three.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.three.username}"
+                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} 𝐴𝑠𝑠𝑖𝑠𝑡𝑎𝑛𝑡 𝑇𝒉𝑟𝑒𝑒 𝑆𝑡𝑎𝑟𝑡𝑒𝑑 :**\n\n✨ 𝐼𝑑 : `{self.three.id}`\n𝑁𝑎𝑚𝑒 : {self.three.name}\n💫 𝑈𝑠𝑒𝑟𝑛𝑎𝑚𝑒 : @{self.three.username}"
                 )
             except:
                 LOGGER(__name__).error(
@@ -129,8 +129,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("DevilsHeavenMF")
-                await self.four.join_chat("FallenAssociation")
+                await self.four.join_chat("full_on_bakchodii")
+                await self.four.join_chat("makima_updatess")
             except:
                 pass
             assistants.append(4)
@@ -143,7 +143,7 @@ class Userbot(Client):
 
             try:
                 await self.four.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ғᴏᴜʀ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.four.id}`\n❄ ɴᴀᴍᴇ : {self.four.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.four.username}"
+                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} 𝐴𝑠𝑠𝑖𝑠𝑡𝑎𝑛𝑡 𝐹𝑜𝑢𝑟 𝑆𝑡𝑎𝑟𝑡𝑒𝑑 :**\n\n✨ 𝐼𝑑 : `{self.four.id}`\n❄ 𝑁𝑎𝑚𝑒 : {self.four.name}\n💫 𝑈𝑠𝑒𝑟𝑛𝑎𝑚𝑒 : @{self.four.username}"
                 )
             except:
                 LOGGER(__name__).error(
@@ -156,8 +156,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("DevilsHeavenMF")
-                await self.five.join_chat("FallenAssociation")
+                await self.five.join_chat("full_on_bakchodii")
+                await self.five.join_chat("makima_updatess")
             except:
                 pass
             assistants.append(5)
@@ -170,7 +170,7 @@ class Userbot(Client):
 
             try:
                 await self.five.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ғɪᴠᴇ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.five.id}`\n❄ ɴᴀᴍᴇ : {self.five.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.five.username}"
+                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} 𝐴𝑠𝑖𝑠𝑡𝑎𝑛𝑡 𝐹𝑖𝑣𝑒 𝑆𝑡𝑎𝑟𝑡𝑒𝑑 :**\n\n✨ 𝐼𝑑 : `{self.five.id}`\n❄ 𝑁𝑎𝑚𝑒 : {self.five.name}\n💫 𝑈𝑠𝑒𝑟𝑛𝑎𝑚𝑒 : @{self.five.username}"
                 )
             except:
                 LOGGER(__name__).error(
