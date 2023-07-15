83ab8c4583e7bf70be8c8995fdc8a39bfc8bd42b
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2023-07-15 21:30:53 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -9,23 +9,23 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeybᴏardButton(
-                text="𝐴𝑑𝑑 𝑀𝑒 𝑇𝑜 𝑌𝑜𝑢𝑟 𝐺𝑟𝑜𝑢𝑝",
+                text="𝐴𝑑𝑑 𝑀𝑒 𝑇𝑜 𝑌𝑜𝑢𝑟 𝐺𝑟𝑜𝑢𝑝 ➕",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="𝑀𝑎𝑖𝑛𝑡𝑎𝑖𝑛𝑒𝑟",
+                text="𝑂𝑤𝑛𝑒𝑟 💸",
                 url=f"https://t.me/sleeping_alwasy",
             )
         ],
         [
             InlineKeyboardButton(
-                text="𝐻𝑒𝑙𝑝",
+                text="𝐻𝑒𝑙𝑝 ❓",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="𝑆𝑒𝑡𝑡𝑖𝑛𝑔𝑠", callback_data="settings_helper"
+                text="𝑆𝑒𝑡𝑡𝑖𝑛𝑔𝑠 ⚙", callback_data="settings_helper"
             ),
         ],
      ]
@@ -36,18 +36,18 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="𝐴𝑑𝑑 𝑀𝑒 𝑇𝑜 𝑌𝑜𝑢𝑟 𝐺𝑟𝑜𝑢𝑝",
+                text="𝐴𝑑𝑑 𝑀𝑒 𝑇𝑜 𝑌𝑜𝑢𝑟 𝐺𝑟𝑜𝑢𝑝 ➕",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="𝐻𝑒𝑙𝑝 𝐴𝑏𝑑 𝐶𝑜𝑚𝑚𝑎𝑛𝑑𝑠", callback_data="settings_back_helper"
+                text="𝐻𝑒𝑙𝑝 𝐴𝑛𝑑 𝐶𝑜𝑚𝑚𝑎𝑛𝑑𝑠 ❓", callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="𝑀𝑎𝑖𝑛𝑡𝑎𝑖𝑛𝑒𝑟", url=f"https://t.me/sleeping_alwasy"
+                text="𝑂𝑤𝑛𝑒𝑟 💸", url=f"https://t.me/sleeping_alwasy"
             )
             ],
      ]
